8b974fbf1b2142cb04e6f88cc7927d21ac01a999
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2021-05-28 02:59:36 +0200

    plot 3d

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 import os
 from sklearn import svm, metrics
-
+import pandas as pd
 
 def read_file_in_dir(directory):
     docs = []
@@ -26,7 +26,7 @@ all_docs = pos_docs + neg_docs
 x_train, x_test, y_train, y_test = train_test_split(all_docs, y_data)
 
 Tfidf_Vectorizer = TfidfVectorizer(use_idf=True, stop_words='english')
-tfs = Tfidf_Vectorizer.fit_transform(x_train).astype('float64')
+tfs_training = Tfidf_Vectorizer.fit_transform(x_train).astype('float64')
 
 
 # Create a svm Classifier
@@ -34,31 +34,47 @@ clf = svm.SVC(kernel='linear') # Linear Kernel
 
 
 # Train the model using the training sets
-clf.fit(tfs, y_train)
+clf.fit(tfs_training, y_train)
+
+
 
 
 # Predict the response for test dataset
-tfs = Tfidf_Vectorizer.transform(x_test).astype('float64')
-y_pred = clf.predict(tfs)
+tfs_testing = Tfidf_Vectorizer.transform(x_test).astype('float64').todense()
+y_pred = clf.predict(tfs_testing)
 
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
-#tfs = Tfidf_Vectorizer.transform(["Iam very Sad"]).astype('float64')
-#print(clf.predict(tfs))
+#case = Tfidf_Vectorizer.transform(["Iam very Sad"]).astype('float64')
+#print(clf.predict(case))
+
+vec =  clf.support_vectors_
 
 
-import pylab as pl
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits import mplot3d
 
-print(tfs)
+clf = TruncatedSVD(1000)
+Xpca = clf.fit_transform(tfs_testing)
 
+pca = PCA()
+pca.fit_transform(Xpca)
+pca_variance = pca.explained_variance_
 
-for i in range(0, tfs.shape[0]):
-    if y_test[i] == 1:
-       # print(tfs[i,2])
-        c1 = pl.scatter(tfs[i, 0], tfs[i, 1], c='r', marker='+')
-    elif y_test[i] == 0:
-        c2 = pl.scatter(tfs[i, 0], tfs[i, 1], c='g', marker='o')
+pca2 = PCA(n_components=10)
+pca2.fit(Xpca)
+x_3d = pca2.transform(Xpca)
 
-pl.title('Iris training dataset with 3 classes and    known outcomes')
-pl.show()
+# Creating figure
+fig = plt.figure(figsize=(7, 7))
+ax = plt.axes(projection="3d")
 
+# Creating plot
+ax.scatter3D(x_3d[:,0], x_3d[:,5], x_3d[:,9], c=y_test)
+plt.title("simple 3D scatter plot")
+
+# show plot
+plt.show()
